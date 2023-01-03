36c208c857117a760f689b332c1901bc608c5c9d
Angelos Zaimis <angelos.zaimis.dev@gmail.com>
2023-01-03 17:53:23 +0100

    mapp added

--- js/scripts.js
+++ js/scripts.js
@@ -375,3 +375,21 @@ Pace.on('done', function() {
     })
 
    
+
+    // Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 47.377830, lng: 8.528520 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
